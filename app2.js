3355a6317a9d2897401a87085d22b5c643546cd5
brettmadrid <brettmadrid9@gmail.com>
2020-06-06 22:38:51 -0700

    wired up buttion functionality

--- app2.js
+++ app2.js
@@ -4,16 +4,38 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: [],
   },
   methods: {
     startGame: function () {
       this.gameIsRunning = true
       this.playerHealth = 100
       this.monsterHealth = 100
+      this.turns = []
     },
     attack: function () {
-      this.monsterHealth -= this.calculateDamage(3, 10)
-      if (this.checkWin()) return
+      var damage = this.calculateDamage(3, 10)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster for ' + damage,
+      })
+      if (this.checkWin()) {
+        return
+      }
+
+      this.monsterAttacks()
+    },
+    specialAttack: function () {
+      var damage = this.calculateDamage(10, 20)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits Monster hard for ' + damage,
+      })
+      if (this.checkWin()) {
+        return
+      }
       this.monsterAttacks()
     },
     heal: function () {
@@ -22,19 +44,23 @@ new Vue({
       } else {
         this.playerHealth = 100
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10',
+      })
       this.monsterAttacks()
-    },
-    specialAttack: function () {
-      this.monsterHealth -= this.calculateDamage(10, 20)
-      if (this.checkWin()) return
-      this.monsterAttacks()
-    },
-    monsterAttacks: function () {
-      this.playerHealth -= this.calculateDamage(5, 12)
-      this.checkWin()
     },
     giveUp: function () {
       this.gameIsRunning = false
+    },
+    monsterAttacks: function () {
+      var damage = this.calculateDamage(5, 12)
+      this.playerHealth -= damage
+      this.checkWin()
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits Player for ' + damage,
+      })
     },
     calculateDamage: function (min, max) {
       return Math.max(Math.floor(Math.random() * max) + 1, min)
@@ -48,7 +74,7 @@ new Vue({
         }
         return true
       } else if (this.playerHealth <= 0) {
-        if (confirm('You lost :( New Game?')) {
+        if (confirm('You lost! New Game?')) {
           this.startGame()
         } else {
           this.gameIsRunning = false

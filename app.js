979be4f4c375d3c05ddb33e86942c12fdf6b7533
brettmadrid <brettmadrid9@gmail.com>
2020-06-07 17:16:05 -0700

    styled logging section

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ new Vue({
       this.gameIsRunning = true
       this.playerHealth = 100
       this.monsterHealth = 100
+      this.turns = []
     },
     attack: function () {
       let damage = this.calculateDamage(3, 10)
@@ -28,10 +29,19 @@ new Vue({
       } else {
         this.playerHealth = 100
       }
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player heals for 10',
+      })
       this.monsterAttacks()
     },
     specialAttack: function () {
-      this.monsterHealth -= this.calculateDamage(10, 20)
+      let damage = this.calculateDamage(10, 20)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits monster hard for ' + damage,
+      })
       if (this.checkWin()) return
       this.monsterAttacks()
     },

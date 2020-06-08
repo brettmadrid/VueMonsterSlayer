cae641ee8156f3a429243567c3a9ec59fcafbe11
brettmadrid <brettmadrid9@gmail.com>
2020-06-07 17:01:27 -0700

    rendered turns results conditionally

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ new Vue({
     playerHealth: 100,
     monsterHealth: 100,
     gameIsRunning: false,
+    turns: [],
   },
   methods: {
     startGame: function () {
@@ -12,7 +13,12 @@ new Vue({
       this.monsterHealth = 100
     },
     attack: function () {
-      this.monsterHealth -= this.calculateDamage(3, 10)
+      let damage = this.calculateDamage(3, 10)
+      this.monsterHealth -= damage
+      this.turns.unshift({
+        isPlayer: true,
+        text: 'Player hits monster for ' + damage,
+      })
       if (this.checkWin()) return
       this.monsterAttacks()
     },
@@ -30,7 +36,12 @@ new Vue({
       this.monsterAttacks()
     },
     monsterAttacks: function () {
-      this.playerHealth -= this.calculateDamage(5, 12)
+      let damage = this.calculateDamage(5, 12)
+      this.playerHealth -= damage
+      this.turns.unshift({
+        isPlayer: false,
+        text: 'Monster hits player for ' + damage,
+      })
       this.checkWin()
     },
     giveUp: function () {
